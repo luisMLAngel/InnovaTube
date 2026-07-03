dd3700a29bbc0580f69abf1a14ddbd705ac1f39a
Luis Angel Mendoza Lucio <luisangelmendozalucio@gmail.com>
2026-07-03 12:22:12 -0600

    agregacion de subfijo para la url del backend

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  SERVER: 'https://innova-tube-backend.vercel.app',
+  SERVER: 'https://innova-tube-backend.vercel.app/api/v1',
   YOUTUBE_API_KEY: 'AIzaSyAWkOPF89dhszm1efb5ElGPwlbl0zjnM2E',
   PRODUCTION: false,
 };
